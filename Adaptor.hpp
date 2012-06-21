ccad092d17c2d1511c75ee958edcb01e79fb28cf
Johnny31525 <Johnny31525@gmail.com>
2012-06-21 13:29:44 +0000

    add makefile, include json parser, add PHP outputFile, and update main

--- Adaptor.hpp
+++ Adaptor.hpp
@@ -1,29 +1,39 @@
+//ifndef .......
+
 #include <stdio.h>
 #include <stdlib.h>
 
-
+#include <fstream>
 #include<iostream>
 #include<string>
+
+#include "json/json.h"
 
 using namespace std;
 
 class Adaptor{
 	public:
-		virtual void getFriendList() = 0;
-		virtual void getJason(string s) = 0;
+		virtual void getFriendList(Json::Value& friendJson) = 0;
+		virtual void getJson(string s) = 0;
 };
 
 class FacebookAdaptor :public Adaptor{
 	public:
-		virtual void getFriendList(){
-			system("php ./fetch_data.php friends");
+		virtual void getFriendList(Json::Value& friendJson){
+			system("php ./fetch_data.php friends tmp.json");
+			std::ifstream file("tmp.json");
+			file >> friendJson;
 		}
-		virtual void getJason(string s){}
+		virtual void getJson(string s){
+			int i = 0;
+		}
 
 };
+/*
 class TwitterAdaptor:public Adaptor{
 	public:
 		virtual void getFriendList(){}
-		virtual void getJason(string s){}
+		virtual void getJson(string s){}
 
 };
+*/
