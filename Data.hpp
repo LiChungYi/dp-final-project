6f4468ee40dd3e8ddeb469bf079a7ec7637d6687
Johnny31525 <Johnny31525@gmail.com>
2012-06-22 07:43:59 +0000

    Now we can list the friend names

---
+++ Data.hpp
@@ -0,0 +1,49 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <iostream>
+#include "Adapter.hpp"
+
+#include<stdlib.h>
+
+using namespace std;
+
+class FacebookData{
+	private:
+		string accessToken;
+		FacebookAdapter faceboookAdapter;
+	public:
+		FacebookData(string in_accessToken):accessToken(in_accessToken), faceboookAdapter(in_accessToken){}
+		string getMyID(){
+			Json::Value theJson;
+			faceboookAdapter.getMyJson("profile", theJson);
+			return theJson["id"].asString();
+		}
+
+		vector<string> getMyFriendIDList(){
+			Json::Value theJson;
+			faceboookAdapter.getMyJson("friends", theJson);
+			
+			vector<string> ret;
+			for(unsigned i = 0; i < theJson["data"].size(); ++i){
+				ret.push_back(theJson["data"][i]["id"].asString());
+			}
+			return ret;
+		}
+
+		
+		vector<string> getMyFriendNameList(){
+			Json::Value theJson;
+			faceboookAdapter.getMyJson("friends", theJson);
+			
+			vector<string> ret;
+			for(unsigned i = 0; i < theJson["data"].size(); ++i){
+				ret.push_back(theJson["data"][i]["name"].asString());
+			}
+			return ret;
+		}
+
+};
+
+
+#endif //DATA_HPP
--- fetch_data.php
+++ fetch_data.php
@@ -3,22 +3,19 @@
 //if(strcmp($object,"friend")==0)
 //	$object="friends";
 
-$object=$argv[1];
-$outputFile=$argv[2];
+$access_token=$argv[1];
+$object=$argv[2];
+if(strcmp($object, "profile") == 0)
+	$object = "";
+$person=$argv[3];
+$outputFile=$argv[4];
 //header("Content-Type:text/html; charset=utf-8");
 
-$app_id =  '346899418712471';
-$app_secret =  'de58ce8f0e3ca5b288a219b72322ba9f';
-$my_url = "http://www.csie.ntu.edu.tw/~b96102/facebook/example.php?object=".$object;
-$scope = "read_friendlists,read_stream,user_status";
+
+//$token = trim(file_get_contents("token.txt"));
 
 
-$token = trim(file_get_contents("token.txt"));
-
-
-
-
-$url = "https://graph.facebook.com/me/".$object."?access_token=" . $token;	
+$url = "https://graph.facebook.com/". $person ."/".$object."?access_token=" . $access_token;	
 $json = file_get_contents($url);
 $decoded_json =  json_decode($json);
 //print_r($decoded_json);
