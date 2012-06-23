95938ff8466acab648990492cebc173586be4c9a
yishiang.Tzeng@gmail.com <yishiang.Tzeng@gmail.com@d8b87a11-e372-e54b-8c90-38587cce01bd>
2012-06-23 09:53:08 +0000

    after debug

--- trunk/User.hpp
+++ trunk/User.hpp
@@ -13,6 +13,19 @@ class User{
 		string relationship;
 		string birthday;
 		string location;
-		User(string name = "", string id = "", string gen = "", string lan = "", string rela = "", string birth = "", string loc = ""):userName(name),ID(id),gender(gen),language(lan),relationship(rela),birthday(birth),location(loc){}
+		User(string name = "", string id = "", string gen = "", string lan = "", string rela = "", string birth = "", string loc = ""):
+		userName(name),ID(id),gender(gen),language(lan),relationship(rela),birthday(birth),location(loc){}
+		friend ostream& operator<<(ostream&, const User&);
+
 };
+ostream& operator<<(ostream& os, const User& u){
+	os<<"userName : "<<u.userName<<endl;
+	os<<"ID : "<<u.ID<<endl;
+	os<<"gender : "<<u.gender<<endl;
+	os<<"language : "<<u.language<<endl;
+	os<<"relationship : "<<u.relationship<<endl;
+	os<<"birthday : "<<u.birthday<<endl;
+	os<<"location : "<<u.location<<endl;
+	return os;
+}
 #endif
