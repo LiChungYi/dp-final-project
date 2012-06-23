81d658e9a50cefe91e65b1cd2544a4476690473f
yishiang.Tzeng@gmail.com <yishiang.Tzeng@gmail.com@d8b87a11-e372-e54b-8c90-38587cce01bd>
2012-06-23 09:17:04 +0000

    add cout

--- trunk/Post.hpp
+++ trunk/Post.hpp
@@ -1,5 +1,7 @@
 #ifndef POST_HPP
 #define POST_HPP
+#include<iostream>
+using namespace std;
 
 class Post{
 	public:
@@ -11,7 +13,16 @@ class Post{
 		string place;
 		Post(string in_postID, string in_fromID, string in_content, string in_createdTime, string in_nLike, string in_place):
 			postID(in_postID), fromID(in_fromID), content(in_content), createdTime(in_createdTime), nLike(in_nLike), place(in_place){}
+		friend ostream& operator<<(ostream&, const Post&);
 
 };
+ostream& operator<<(ostream& os, const Post& p){
+	os<<"postID : "<<p.postID<<endl;
+	os<<"fromID : "<<p.fromID<<endl;
+	os<<"content : "<<p.content<<endl;
+	os<<"createdTime : "<<p.createdTime<<endl;
+	os<<"nLIke : "<<p.nLike<<endl;
+	os<<"place : "<<p.place<<endl;
+}
 
 #endif //POST_HPP
