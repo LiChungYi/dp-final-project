c24610ea77cb5a6ab75c635070a81a7eeae9236d
yishiang.Tzeng@gmail.com <yishiang.Tzeng@gmail.com@d8b87a11-e372-e54b-8c90-38587cce01bd>
2012-06-23 03:57:44 +0000

    adding a temporary file test.php

---
+++ trunk/test.php
@@ -0,0 +1,38 @@
+<?php
+
+// Read in our saved access token/secret
+$accessToken = file_get_contents("./Twitter/access_token");
+$accessTokenSecret = file_get_contents("./Twitter/access_token_secret");
+// Create our twitter API object
+require_once("./Twitter/twitteroauth/twitteroauth.php");
+$oauth = new TwitterOAuth('1UvPUPqi6h90qARceGDvA','NNvj1cIRLdI1CGccFH5qjOayFT1GUA4UX2bf8cQVD4', $accessToken, $accessTokenSecret);
+// Send an API request to verify credentials
+$credentials = $oauth->get("account/verify_credentials");
+//print_r($credentials);
+echo "Connected as @" . $credentials->screen_name;
+
+// Post our new "hello world" status
+$oauth->post('statuses/update', array('status' => "test post 2"));
+
+
+printf("<hr/>");
+$oauth->format = 'json';
+$tweets = $oauth->get('statuses/user_timeline');
+file_put_contents('tweet.json', json_encode($tweets));
+chmod ("tweet.json" , 0755);
+
+/*printf("<hr/>");
+$tweets = $oauth->get('statuses/user_timeline');
+$num = count($tweets);
+for($i=0;$i<$num;$i++){
+	printf("%s %s<br>",$tweets[$i]->id,$tweets[$i]->text);
+	$oauth->post('statuses/update', array('status' => "@common321 good".$i,'in_reply_to_status_id'=>$tweets[$i]->id."@common321"));
+	printf("<br>");
+}
+*/
+
+printf("<hr/>");
+$friend = $oauth->get('friends/ids');
+print_r($friend);
+?>
+
