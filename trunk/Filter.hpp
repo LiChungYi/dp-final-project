0000b2f6934b55a6f6d61ce5ae039de6355a8ba1
yishiang.Tzeng@gmail.com <yishiang.Tzeng@gmail.com@d8b87a11-e372-e54b-8c90-38587cce01bd>
2012-06-24 16:02:44 +0000

    new filter

--- trunk/Filter.hpp
+++ trunk/Filter.hpp
@@ -3,7 +3,9 @@
 
 
 #include <map>
+#include<assert.h>
 #include "Data.hpp"
+#include <sstream>
 
 using namespace std;
 
@@ -12,17 +14,19 @@ using namespace std;
 template <class T>	//T can be Post or User
 class Filter{
 	private:
-		static map<string, Filter*> filterManager;
+		static map<string, Filter<T>*> filterManager;
 	public:
 		Filter(string className){
+			cerr<<className<<endl;
 			filterManager.insert(make_pair(className,this));
+			cerr<<filterManager.size()<<endl;
 		}
-		Filter(){
+		Filter(){}
+		static Filter* newFilter(string className, string condition){
+			Filter* f = (*filterManager.find(className)).second;
+			return f->Clone(condition);	
 		}
-		static Filter* newFilter(string& className){
-			
-		}
-		Filter* Clone() {}
+		virtual Filter* Clone(string s) =0;
 		virtual bool shouldKeep(T) = 0;		
 };
 
@@ -31,6 +35,7 @@ class PostContentFilter:public Filter<Post>{
 	private:
 		string shouldBeContained;
 	public:
+//		static PostContentFilter dummy;
 		PostContentFilter(string in_shouldBeContained):shouldBeContained(in_shouldBeContained){}
 
 		virtual bool shouldKeep(Post p){
@@ -40,8 +45,8 @@ class PostContentFilter:public Filter<Post>{
 		}
 		PostContentFilter():Filter<Post>("PostContentFilter"){
 		}
-		Filter<Post>* Clone(){
-			return new PostContentFilter(*this);
+		Filter<Post>* Clone(string s){
+			return new PostContentFilter(s);
 
 		}
 };
@@ -50,6 +55,7 @@ class PostFromIDFilter:public Filter<Post>{
 	private:
 		string theFromID;
 	public: 
+//		static PostFromIDFilter dummy;
 		PostFromIDFilter(string in_theFromID):theFromID(in_theFromID){}
 		virtual bool shouldKeep(Post p){
 			if(p.fromID.compare(theFromID) == 0)
@@ -58,8 +64,8 @@ class PostFromIDFilter:public Filter<Post>{
 		}
 		PostFromIDFilter():Filter<Post>("PostFromIDFilter"){
 		}
-		Filter<Post>* Clone(){
-			return new PostFromIDFilter(*this);
+		Filter<Post>* Clone(string s){
+			return new PostFromIDFilter(s);
 		}
 };
 
@@ -67,6 +73,7 @@ class PostTimeFilter:public Filter<Post>{
 	private:
 		string fromTime, toTime;
 	public:
+//		static PostTimeFilter dummy;
 		PostTimeFilter(string in_fromTime, string in_toTime):fromTime(in_fromTime), toTime(in_toTime){}
 		virtual bool shouldKeep(Post p){
 			if(fromTime.compare(p.createdTime) <= 0 && p.createdTime.compare(toTime) <= 0 )
@@ -76,8 +83,12 @@ class PostTimeFilter:public Filter<Post>{
 		
 		PostTimeFilter():Filter<Post>("PostTimeFilter"){
 		}
-		Filter<Post>* Clone(){
-			return new PostTimeFilter(*this);
+		Filter<Post>* Clone(string s){
+			stringstream ss(s);
+			string tmp1, tmp2;
+			getline(ss,tmp1,'\t');
+			getline(ss,tmp2,'\t');
+			return new PostTimeFilter(tmp1, tmp2);
 		}
 };
 
@@ -86,6 +97,7 @@ class UserRelationshipStatusFilter:public Filter<User>{
 	private:
 		string relationshipStatus;
 	public:
+//		static UserRelationshipStatusFilter dummy;
 		UserRelationshipStatusFilter(string in_relationshipStatus):relationshipStatus(in_relationshipStatus){}
 		virtual bool shouldKeep(User u){
 			if(u.relationshipStatus.compare(relationshipStatus) == 0)
@@ -94,8 +106,8 @@ class UserRelationshipStatusFilter:public Filter<User>{
 		}
 		UserRelationshipStatusFilter():Filter<User>("UserRelationshipStatusFilter"){
 		}
-		Filter<User>* Clone(){
-			return new UserRelationshipStatusFilter(*this);	
+		Filter<User>* Clone(string s){
+			return new UserRelationshipStatusFilter(s);	
 		}
 };
 
@@ -103,16 +115,17 @@ class UserGenderFilter:public Filter<User>{
 	private:
 		string gender;
 	public:
+//		static UserGenderFilter dummy;
 		UserGenderFilter(string in_gender):gender(in_gender){}
 		virtual bool shouldKeep(User u){
 			if(u.gender.compare(gender) == 0)
 				return true;
 			return false;
 		}
-		UserGenderFilter():Filter<User>("UserRelationshipStatusFilter"){
+		UserGenderFilter():Filter<User>("UserGenderFilter"){
 		}
-		Filter<User>* Clone(){
-			return new UserGenderFilter(*this);
+		Filter<User>* Clone(string s){
+			return new UserGenderFilter(s);
 		}
 };
 
@@ -129,8 +142,9 @@ class AndFilter: public Filter<T>{//Composite Pattern!
 		}
 		AndFilter():Filter<T>("AndFilter"){
 		}
-		Filter<T>* Clone(){
-			return new AndFilter(*this);
+		Filter<T>* Clone(string s){
+			assert(0);
+			return NULL;
 		}
 		~AndFilter(){
 			delete a;
@@ -151,14 +165,23 @@ class OrFilter: public Filter<T>{//Composite Pattern!
 		}
 		OrFilter():Filter<T>("OrFilter"){
 		}
-		Filter<T>* Clone(){
-			return new OrFilter("OrFilter");
+		Filter<T>* Clone(string s){
+			assert(0);
+			return NULL;
 		}
 		~OrFilter(){
 			delete a;
 			delete b;
 		}
 };
+/*
+template<class T> 
+map<string,Filter<T>*> Filter<T>::filterManager;
 
-
+PostContentFilter PostContentFilter::dummy;
+PostFromIDFilter PostFromIDFilter::dummy;
+PostTimeFilter PostTimeFilter::dummy;
+UserGenderFilter UserGenderFilter::dummy;
+UserRelationshipStatusFilter UserRelationshipStatusFilter::dummy;
+*/
 #endif //FILTER_HPP
